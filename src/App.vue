0830f7d542abf3b2373785353fff196f12a747cf
shengxihu <1679112936@qq.com>
2016-09-06 14:14:28 +0800

    init repository cli

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <hello></hello>
+    <p>
+      Welcome to your Vue.js app!
+    </p>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style lang='scss'>
+@import '../static/home.scss';
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  background: $_blue;
+}
+
+#app {
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="test"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
+<style lang='scss'>
+  @import '../../static/mixins/_sprite.scss';
+
+  .test {
+    @include sprite($logo);
+  }
+</style>
