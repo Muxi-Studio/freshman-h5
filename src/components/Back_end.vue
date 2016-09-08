e2183c6e59f308143ecb1cdb617a139a9a1b50d3
shengxihu <1679112936@qq.com>
2016-09-08 17:27:37 +0800

    first push

---
+++ src/components/Back_end.vue
@@ -0,0 +1,79 @@
+<template>
+	<div class="back_end page">
+		<div class="mian">
+			<div class="back_end_ship"></div>
+			<div class="group sub color_red">
+				<span class="stress color_yellow">后台组</span>—开启全方位支援
+			</div>
+			<div class="txt sub color_red">
+				<div class="close">
+					在团队中他着力于给
+				</div>
+				<div class="close">
+					产品开发<span>提供</span>强有力的
+				</div>
+				<div class="close stress">
+					后备资源
+				</div>
+				<div class="close">
+					容易被忽略
+				</div>
+				<div>
+					却是在产品开发中不可缺的
+				</div>
+				<div class="stress">中间力量</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss'>
+@import '../../static/common.scss';
+@import '../../static/mixins/_sprite.scss';
+
+.back_end {
+	width: 100%;
+	height: 100%;
+	.mian {
+		height: y_size(1285);
+		width: 100%;
+		@include _sprite($home_ship_line,1285px);
+	}
+	.back_end_ship {
+		height: 55.56%;
+		width: 100%;
+		@include _sprite($back_end_ship,714px);
+	}
+	.group {
+		padding-bottom: y_size(90);
+	}
+	.txt {
+		line-height: 1;
+		padding-left: x_size(100);
+	}
+	.stress {
+		font-size: .96rem;
+	}
+	.color_yellow {
+		color: rgb(255, 231, 95);
+	}
+	.sub {
+		font-size: .64rem;
+	}
+	.color_red {
+		color: rgb(255, 115, 247);
+	}
+	.close {
+		line-height: .9;
+	}
+	.group {
+		text-align: center;
+	}
+}
+</style>
---
+++ src/components/Hack.vue
@@ -0,0 +1,77 @@
+<template>
+	<div class="hack page">
+		<div class="mian">
+			<div class="ship_light"></div>
+			<div class="txt">
+				<div class="watchword">
+					BORN TO HACK
+				</div>
+				<div class="for">
+					2016木犀秋季招新
+				</div>
+				<div class="for">
+					寻找为HACK而生的你
+				</div>
+				<div class="belief">
+					我们在路上 前方不会太远
+				</div>
+				<div class="enter">
+					我要报名
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss'>
+@import '../../static/common.scss';
+@import '../../static/mixins/_sprite.scss';
+.hack {
+	width: 100%;
+	height: 100%;
+	.mian {
+		height: 100%;
+		width: 100%;
+		display: inline-block;
+		@include _sprite($hack_ship,1334px);
+	}
+	.ship_light {
+		height: y_size(645);
+		margin-top: y_size(96);
+		width: 100%;
+		@include _sprite($ship_light,645px);
+	}
+	.txt {
+		padding-top: y_size(425);
+	}
+	.watchword,.for,.belief,.enter {
+		font-size: 0.64rem;
+		text-align: center;
+	}
+	.watchword {
+		line-height: 1.146;
+		color: rgb(196, 199, 10);
+		padding-bottom: y_size(25);
+	}
+	.for {
+		line-height: .8;
+		color: rgb(0, 255, 72);
+	}
+	.belief {
+		line-height: 1.146;
+		color: rgb(196, 199, 10);
+		padding-top: y_size(25);
+	}
+	.enter {
+		line-height: 1.146;
+		color: rgb(255, 141, 41);
+		padding-top: y_size(220);
+	}
+}
+</style>
